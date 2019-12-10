5f2f9ce14a1232ca8312bb3dab8094c52b0736a2
Martin Salinas Scussolin <marthoxx@gmail.com>
2019-12-10 18:35:45 -0300

    aa xd

--- Tarea2/test.cpp
+++ Tarea2/test.cpp
@@ -8,15 +8,9 @@
 #include <algorithm>
 using namespace std;
 
-//definicion de tipos
-typedef vector<int> number;
 
-void print(std::vector<int> const &input)
-{
-	for (int i = 0; i < input.size(); i++)
-	{
-		std::cout << input.at(i) << ' ';
-	}
+int multiplicar(int digits, string num1, string num2){
+
 }
 
 int main(int argc, char **argv)
@@ -25,23 +19,6 @@ int main(int argc, char **argv)
 	int digits = 0;
 	string num1;
 	string num2;
-	//cantidad de char en una linea lmao
-	int doubleD = 0;
-	//contador
-	int i = 0;
-	//cantidad de vertices
-	number first;
-	// vertice del grafo
-	number second;
-
-	//bits
-	int bit = 0;
-
-	//dummy thing
-	char dummy;
-
-	//linea
-	string line; 
 
 	//leer cantidad de bits de multiplicacion
 	while (cin >> digits){
@@ -51,6 +28,7 @@ int main(int argc, char **argv)
 		//get numero 2
 		cin >> num2;
 		cout << "\nayy lmaao\n" << num1 << "\n" << num2 << "\nayy lmaao\n";
+		cout << multiplicar(digits, num1, num2) << "\n";
 
 	}
 
